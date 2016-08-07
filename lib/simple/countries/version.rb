0d823b6f7b718cd0b4e3d0fa3f0c865ea6bb9c07
Quan Nguyen <quanmn.libra@gmail.com>
2016-08-07 15:28:11 +0700

    initialize commit

---
+++ lib/simple/countries/version.rb
@@ -0,0 +1,5 @@
+module Simple
+  module Countries
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/simple/countries_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Simple::Countries do
+  it 'has a version number' do
+    expect(Simple::Countries::VERSION).not_to be nil
+  end
+
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
+end
